87c6e3e2dd379c0f9ffd38afca426b7a721e6076
meron37 <134938986+meron37@users.noreply.github.com>
2025-12-02 18:42:46 -0600

    Exercise 2: Display ProductID, ProductName, UnitPrice, UnitsInStock of each product sold by Northwind

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,36 @@
+//
+//// ============================= Exercise 1 =============================
+//package com.pluralsight;
+//
+//import java.sql.*;
+//
+//public class Main {
+//    public static void main(String[] args) throws SQLException {
+//
+//        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind","root","yearup24");
+//
+//        //  Prepare the statement
+//        Statement statement = connection.createStatement();
+//
+//        // Execute query
+//        ResultSet resultSet = statement.executeQuery("SELECT * FROM northwind.Products;");
+//
+//        // Print results
+//        while (resultSet.next()) {
+//            String name = resultSet.getString("ProductName");
+//            System.out.println(name);
+//        }
+//
+//        // Close connection
+//
+//        connection.close();
+//    }
+//}
 
-// === Exrecise 1
+
+// =========================================== Exercise 2====================
+
+
 package com.pluralsight;
 
 import java.sql.*;
@@ -13,12 +44,17 @@ public class Main {
         Statement statement = connection.createStatement();
 
         // Execute query
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM northwind.Products;");
+        ResultSet resultSet = statement.executeQuery("SELECT ProductID, ProductName, UnitPrice, UnitsInStock  FROM Products;");
 
         // Print results
         while (resultSet.next()) {
+            int id = resultSet.getInt("ProductID");
             String name = resultSet.getString("ProductName");
-            System.out.println(name);
+            double price = resultSet.getDouble("UnitPrice");
+            int stock = resultSet.getInt("UnitsInStock");
+
+
+            System.out.println(("Product ID:"+id + "\n  "+"Name:" + name+" \n $" +"Price:" + price+" \n stock: " + stock));
         }
 
         // Close connection
